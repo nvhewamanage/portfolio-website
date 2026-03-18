a516b0efa157ce8b80b5573b238ac72dbac30c5b
navodh viduranga <navodhviduranga2002@gmail.com>
2026-03-18 11:42:47 +0530

    update the contact form and newsletter to reset after send the email and after the subscribe

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
